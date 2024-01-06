72a87846d6abd956d1ee7b4d445dedb8a7141a6b
qinfalin <401907589@qq.com>
2024-01-06 23:57:23 +0800

    Initial commit

---
+++ src/build-HelloWorld-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/DeskTop/git/GitHub/Qt_Growing/src/build-HelloWorld-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/HelloWorld_autogen/mocs_compilation.cpp" "CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.obj.d"
+  "D:/DeskTop/git/GitHub/Qt_Growing/src/HelloWorld/main.cpp" "CMakeFiles/HelloWorld.dir/main.cpp.obj" "gcc" "CMakeFiles/HelloWorld.dir/main.cpp.obj.d"
+  "D:/DeskTop/git/GitHub/Qt_Growing/src/HelloWorld/mainwindow.cpp" "CMakeFiles/HelloWorld.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/HelloWorld.dir/mainwindow.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build-HelloWorld-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/HelloWorld.dir/main.cpp.obj"
+  "CMakeFiles/HelloWorld.dir/main.cpp.obj.d"
+  "CMakeFiles/HelloWorld.dir/mainwindow.cpp.obj"
+  "CMakeFiles/HelloWorld.dir/mainwindow.cpp.obj.d"
+  "CMakeFiles/HelloWorld_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/HelloWorld_autogen.dir/ParseCache.txt"
+  "HelloWorld_autogen"
+  "HelloWorld.exe"
+  "HelloWorld.exe.manifest"
+  "HelloWorld.pdb"
+  "libHelloWorld.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
